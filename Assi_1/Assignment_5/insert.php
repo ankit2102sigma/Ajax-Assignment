7fa589f08624d4fdbb5004cb33dc5a4b340c631e
Ankit Arora <ankit2102sigma>
2023-03-16 18:00:44 +0530

    fixed

--- Assi_1/Assignment_5/insert.php
+++ Assi_1/Assignment_5/insert.php
@@ -9,12 +9,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "<script>alert('Invalid First Name')</script>";
         return;
     }
-    
+
     if (!(preg_match('/^(?:[1-9]|10)$/', $rating))) {
         echo "<script>alert('Invalid Rating Name')</script>";
         return;
     }
-    
+
 
 
     $sql = "INSERT INTO movie (Title, Rating) 
@@ -27,23 +27,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
 
-$sql_select = "SELECT * FROM movie";
-$result = $conn->query($sql_select);
+    $sql_select = "SELECT * FROM movie";
+    $result = $conn->query($sql_select);
 
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
 
-    $id = $row['id'];
-    $title = $row["Title"];
-    $rating = $row["Rating"];
+            $id = $row['id'];
+            $title = $row["Title"];
+            $rating = $row["Rating"];
 
-    $return_arr[] = array("id" => $id,
-                    "Title" => $title,
-                    "rating" => $rating);
-}
-echo json_encode($return_arr);
-    
-} else {
-    echo "0 results";
-}
+            $return_arr[] = array(
+                "id" => $id,
+                "Title" => $title,
+                "rating" => $rating
+            );
+        }
+        echo json_encode($return_arr);
+    } else {
+        echo "0 results";
+    }
 }
